e04b4ef8451d317d9541be0f02a93230d02cb373
Moussa Camara <moussa.camara@iknsa.com>
2021-03-19 10:06:22 +0100

    correction exo1

--- Afpa/src/Serie_exercices/Livre.java
+++ Afpa/src/Serie_exercices/Livre.java
@@ -1,36 +1,40 @@
 package Serie_exercices;
 
 public class Livre {
-	/*
-	 * Exercice 1 :
-		1. Définir une classe Livre avec les attributs suivants : Id, Titre, Auteur (Nom
-		complet), Prix.
-		2. Définir les accesseurs aux différents attributs de la classe.
-		3. Définir un constructeur permettant d’initialiser les attributs d'un objet
-		livre par des valeurs saisies par l’utilisateur. Sachant qu’Id doit être autoincrément.
-		4. Définir la méthode toString ( ) permettant d’afficher les informations du livre
-		en cours.
-		5. Écrire un programme testant la classe Livre.
-	 */
-	
-	//J'apporte mon aide :)
-	
-	//1. Définir une classe Livre avec les attributs suivants : 
-	//ATTRIBUTES: Id, Titre, Auteur (Nom complet), Prix.
-	
+	//Les attributs
 	private int id;
 	private String titre;
 	private String auteur;
-	//double: on peut avoir des nombres à virgule
 	private double prix;
+	private static int id_increment = 1;
 	
-	//Création du constructeurs sans paramétres
-	public Livre() {
-		super();
-		//Je lui donne ici des valeurs par defaut
-		this.titre = "Cool le java";
-		this.auteur = "Afpa";
-		this.prix = 99.99;
+	public int getId() {
+		return id;
 	}
 	
+	public String getTitre() {
+		return titre;
+	}
+	
+	public String getAuteur() {
+		return auteur;
+	}
+	
+	public double getPrix() {
+		return prix;
+	}
+
+	public Livre(String titre, String auteur, double prix) {
+		super();
+		this.id = id_increment++;
+		this.titre = titre;
+		this.auteur = auteur;
+		this.prix = prix;
+	}
+
+	@Override
+	public String toString() {
+		return "Livre [id=" + id + ", titre=" + titre + ", "
+				+ "auteur=" + auteur + ", prix=" + prix + "]";
+	}
 }
